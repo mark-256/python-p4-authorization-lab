e722fbea7136f388df98764937026bea7df74fde
Mark <markjohnguto@gmail.com>
2026-01-08 14:41:17 +0300

    modified

--- server/app.py
+++ server/app.py
@@ -85,14 +85,22 @@ class CheckSession(Resource):
         return {}, 401
 
 class MemberOnlyIndex(Resource):
-    
+
     def get(self):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+        articles = Article.query.filter_by(is_member_only=True).all()
+        return [article.to_dict() for article in articles], 200
 
 class MemberOnlyArticle(Resource):
-    
+
     def get(self, id):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+        article = Article.query.filter_by(id=id, is_member_only=True).first()
+        if not article:
+            return {'message': 'Article not found'}, 404
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
